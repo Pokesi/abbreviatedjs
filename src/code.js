49a977d0b48bdaaa4fc9ccad227dcc8223a2f7d3
Pokesi <81540683+Pokesi@users.noreply.github.com>
2022-07-12 13:00:22 +1000

    Remove .js commits

---
+++ src/code.js
@@ -0,0 +1,11 @@
+export const code = `fun main() {
+  fori i<100 efori {
+    lg(i.to_s);
+  }
+}`
+
+export const jscode = `function main() {
+  for(let i = 0; i<100; i++) {
+    console.log(i.toString());
+  }
+}`
--- src/setupTests.js
+++
@@ -1,7 +0,0 @@
-
- // jest-dom adds custom jest matchers for asyncserting on DOM nodes.
- // allows you to do things like:
- //exportect(element).toHaveTextContent(/react/i)
- // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
- 
